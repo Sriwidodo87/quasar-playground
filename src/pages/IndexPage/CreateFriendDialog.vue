9605a0983a0825bf21b9cb3cae68c490f42ab74f
unknown <swidodo87@gmail.com>
2025-05-06 10:07:04 +0800

    tombol validasi

--- src/pages/IndexPage/CreateFriendDialog.vue
+++ src/pages/IndexPage/CreateFriendDialog.vue
@@ -1,14 +1,32 @@
 <script setup>
+import { ref } from "vue";
+
 const form = defineModel("form", { type: Object });
 const emit = defineEmits(["create"]); // mengacu pada createFriendDialog
+
+const errors = ref({});
+
+function giveMeErrors() {
+  errors.value = {
+    name: ["Name must be at least 3 character"],
+  };
+}
 </script>
 <template>
   <q-dialog>
     <q-card>
+      <q-btn @click="giveMeErrors()"> Give Me Errors </q-btn>
+
       <q-card-section>
         <q-form class="q-gutter-y-md" @submit="emit('create')">
           <!-- name: text input -->
-          <q-input v-model="form.name" label="Name" filled />
+          <q-input
+            v-model="form.name"
+            label="Name"
+            filled
+            :error="!!errors.name?.length"
+            :error-message="errors.name?.join(', ')"
+          />
           <!-- email:text input -->
           <q-input v-model="form.email" label="Email" filled />
           <!-- age: input -->
