379ecb36edc8de9675ac34db6d0e86a17b40989e
unknown <swidodo87@gmail.com>
2025-05-13 21:29:25 +0800

    Quasar git System

--- src/pages/IndexPage/IndexPage.vue
+++ src/pages/IndexPage/IndexPage.vue
@@ -18,19 +18,33 @@ function createFriend() {
 const showCreateDialog = ref(false);
 </script>
 <template>
-  <q-page class="row flex-center">
-    <h2 class="text-weight-bolder full-width text-center">Lorem Ipsum</h2>
-    <q-table :columns="columns" :rows="Object.values(friends)" title="Friends">
-      <template #top-right>
-        <q-btn
-          :icon="mdiPlus"
-          round
-          color="primary"
-          @click="showCreateDialog = true"
-        />
-      </template>
-    </q-table>
+  <q-page class="row q-col-gutter-xl">
+    <div class="col-xs-12 col-sm-6 col-lg-4">
+      <q-card>
+        <q-card-section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, libero amet
+          perspiciatis doloribus velit, excepturi molestiae atque quaerat adipisci aliquam
+          at, sed maxime tempora cupiditate unde dicta placeat. Facere, aspernatur.
+        </q-card-section>
+      </q-card>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-lg-4">
+      <q-table :columns="columns" :rows="Object.values(friends)" title="Friends">
+        <template #top-right>
+          <q-btn :icon="mdiPlus" round color="primary" @click="showCreateDialog = true" />
+        </template>
+      </q-table>
+    </div>
 
+    <div class="col-xs-12 col-lg-4">
+      <q-card>
+        <q-card-section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, libero amet
+          perspiciatis doloribus velit, excepturi molestiae atque quaerat adipisci aliquam
+          at, sed maxime tempora cupiditate unde dicta placeat. Facere, aspernatur.
+        </q-card-section>
+      </q-card>
+    </div>
     <CreateFriendDialog
       v-model="showCreateDialog"
       v-model:form="form"
