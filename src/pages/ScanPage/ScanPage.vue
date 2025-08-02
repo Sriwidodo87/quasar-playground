9bc9c1d1bb40b6bff8a64f038d24d99331a83ffd
unknown <swidodo87@gmail.com>
2025-08-02 14:23:02 +0800

    scanning Bluetooth

--- src/pages/ScanPage/ScanPage.vue
+++ src/pages/ScanPage/ScanPage.vue
@@ -1,26 +1,28 @@
 <script setup>
-import { BleClient } from '@capacitor-community/bluetooth-le';
-import {ref} from 'vue';
-const scanResults = ref([])
-
-async function scan () {
-  await	BleClient.initialize()
-
-  await 	BleClient.requestLEScan(
-    {},
-    result =>{
-      scanResults.value.push(result)
-      }
-  )
-}
-
-scan()
+import { useBluetoothScanner } from "src/composables/useBluetoothScanner";
+import { mdiRefresh } from "@quasar/extras/mdi-v7";
+const { scan, scanResults, scanning } = useBluetoothScanner();
 </script>
-
-
 
 <template>
   <q-page padding>
-    <pre>{{ scanResults }}</pre>
+    <q-card>
+      <q-toolbar class="bg-secondary text-white">
+        <q-toolbar-title> Scan For Friends </q-toolbar-title>
+        <q-btn
+          :loading="scanning"
+          round
+          color="white"
+          text-color="grey-10"
+          size="sm"
+          :icon="mdiRefresh"
+          @click="scan()"
+        />
+      </q-toolbar>
+
+      <q-list>
+        <pre>{{ scanResults }}</pre>
+      </q-list>
+    </q-card>
   </q-page>
 </template>
