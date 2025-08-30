9b0344b80d578acc64960da48889a7e9d4c2a693
WithDodo <swidodo87@gmail.com>
2025-08-30 14:17:43 +0800

    q-skeleton

--- src/pages/RampagePage.vue
+++ src/pages/RampagePage.vue
@@ -84,19 +84,46 @@ const tabs =[
 ]
 
 const currentTab= ref('Emails')
+
+const  loading = ref(true)
 </script>
 
 <template>
   <q-page padding>
-    <q-tabs v-model="currentTab">
+    <q-card
+      v-if="loading"
+      style="width: 300px;"
+    >
+      <q-card-section class="row">
+        <q-skeleton type="QAvatar" />
+        <q-skeleton
+          class="col q-ml-md"
+          type="text"
+        />
+        <q-skeleton
+          type="rect"
+          width="300px"
+          height="150px"
+        />
+        <q-card-actions>
+          <q-space />
+          <q-skeleton
+            class="q-mr-md"
+            type="QBtn"
+          />
+          <q-skeleton type="QBtn" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+    <!--  <q-tabs v-model="currentTab">
       <q-tab
         v-for="tab in tabs"
         :key="tab.label"
         :label="tab.label"
         :name="tab.label"
       />
-    </q-tabs>
-    <q-tab-panels
+    </q-tabs> -->
+    <!-- <q-tab-panels
       v-model="currentTab"
       animated
     >
@@ -108,7 +135,7 @@ const currentTab= ref('Emails')
       >
         <pre>{{ tab }}</pre>
       </q-tab-panel>
-    </q-tab-panels>
+    </q-tab-panels> -->
     <!--    <q-infinite-scroll
       :disable="posts.length >=20"
       :offset="250"
